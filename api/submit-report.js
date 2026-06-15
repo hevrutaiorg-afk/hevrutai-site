91215add8903161d4c0b959af0a158388ab33784
HevrutAI <hevrutai.org@gmail.com>
2026-06-15 11:38:00 -0500

    Update submit-report.js

--- api/submit-report.js
+++ api/submit-report.js
@@ -20,7 +20,7 @@ export default async function handler(req, res) {
         Authorization: `Bearer ${AIRTABLE_TOKEN}`,
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(body)
+body: JSON.stringify({ fields: body.fields })
     }
   );
 
